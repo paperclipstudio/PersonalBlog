402f2ad40cb87178bc628bb2b1fc0ae5b6423aac
George Sanger <paperclipstudio@gmail.com>
2022-07-18 12:25:35 +0000

    Fixing warnings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 #[macro_use] extern crate rocket;
 use rocket::fs::{FileServer, relative};
 
-use std::path::{Path, PathBuf};
-use rocket::fs::NamedFile;
-
-
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", FileServer::from(relative!("static")))
